d5d6372eadcdb941bf87985359bd524f3c47813e
Adam Wang <ajw054@ucsd.edu>
2019-02-13 11:09:11 -0800

    Using aiohttp instead of urllib

--- modules/splatnet.py
+++ modules/splatnet.py
@@ -1,55 +1,59 @@
 import json
-import urllib.request
+import aiohttp
+import asyncio
+
 
 class Splatnet:
 
-    def get_json_schedule(self):
-        req = urllib.request.Request(
-            url="https://splatoon2.ink/data/schedules.json",
-            data=None,
-            headers={
-                'User-Agent': 'SplatBot/1.0 Discord: AdamWang#1770'
-            }
-        )
-        with urllib.request.urlopen(req) as url:
-            json_data = json.loads(url.read().decode())
-            return json_data
+    def __init__(self, session=None):
+        if session is None:
+            self.connection = aiohttp.ClientSession()
+        else:
+            self.connection = session
 
-    def get_json_salmon(self):
-        req = urllib.request.Request(
-            url="https://splatoon2.ink/data/coop-schedules.json",
-            data=None,
-            headers={
-                'User-Agent': 'SplatBot/1.0 Discord: AdamWang#1770'
-            }
-        )
-        with urllib.request.urlopen(req) as url:
-            json_data = json.loads(url.read().decode())
-            return json_data
+    async def send_request(self, request, return_raw_and_json=False):
+        """
+        Returns the JSON of a request to a Splatoon2.ink endpoint, or returns the error code as JSON if unsuccessful
+        """
+        header = { "User:Agent": "SplatBot/1.0 Github: github.com/ktraw2/SplatBot"}
+        async with self.connection.get("https://splatoon2.ink/data/" + request + ".json", header=header) as response:
+            if response.status == 200:
+                if return_raw_and_json:
+                    text = await response.text()
+                    return json.loads(text), text
+                else:
+                    return json.loads(await response.text())
+            elif response.status == 429:
+                # bot is sending too many requests, try again after a couple seconds
+                print("Bot is being rate limited by Splatoon2.ink, resending request...")
+                await asyncio.sleep(5)
+                return await self.send_request(request, return_raw_and_json=return_raw_and_json)
+            else:
+                error_string = '{"error":' + str(response.status) + '}'
+                if return_raw_and_json:
+                    return json.loads(error_string), error_string
+                else:
+                    return json.loads(error_string)
 
-    def get_json_gear(self):
-        req = urllib.request.Request(
-            url="https://splatoon2.ink/data/merchandises.json",
-            data=None,
-            headers={
-                'User-Agent': 'SplatBot/1.0 Discord: AdamWang#1770'
-            }
-        )
-        with urllib.request.urlopen(req) as url:
-            json_data = json.loads(url.read().decode())
-            return json_data
+    async def get_turf(self):
+        return await self.send_request("schedules")#['regular']
 
-    def get_json_splatfest(self):
-        req = urllib.request.Request(
-            url="https://splatoon2.ink/data/festivals.json",
-            data=None,
-            headers={
-                'User-Agent': 'SplatBot/1.0 Discord: AdamWang#1770'
-            }
-        )
-        with urllib.request.urlopen(req) as url:
-            json_data = json.loads(url.read().decode())
-            return json_data
+    async def get_ranked(self):
+        return await self.send_request("schedules")#['gachi']
+
+    async def get_league(self):
+        return await self.send_request("schedules")#['league']
+
+    async def get_salmon_detail(self):
+        return await self.send_request("coop-schedules")#['details']
+
+    async def get_salmon_schedule(self):
+        return await self.send_request("coop-schedules")#['schedules']
+
+    async def get_na_splatfest(self):
+        return await self.send_request("festivals.json")#['na']
 
 
+test = Splatnet()
+print(test.get_turf())
 
