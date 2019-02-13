9cbf343376a76579a718623adc6b84674f5344dd
Adam Wang <ajw054@ucsd.edu>
2019-02-13 13:07:50 -0800

    Started json parser

--- modules/splatnet.py
+++ modules/splatnet.py
@@ -1,6 +1,7 @@
-import json
 import aiohttp
 import asyncio
+import json
+# TODO: Fix weird syntax error w/ import statement
 
 
 class Splatnet:
@@ -15,7 +16,7 @@ class Splatnet:
         """
         Returns the JSON of a request to a Splatoon2.ink endpoint, or returns the error code as JSON if unsuccessful
         """
-        header = { "User:Agent": "SplatBot/1.0 Github: github.com/ktraw2/SplatBot"}
+        header = {"User:Agent": "SplatBot/1.0 Github: github.com/ktraw2/SplatBot"}
         async with self.connection.get("https://splatoon2.ink/data/" + request + ".json", header=header) as response:
             if response.status == 200:
                 if return_raw_and_json:
@@ -36,24 +37,22 @@ class Splatnet:
                     return json.loads(error_string)
 
     async def get_turf(self):
-        return await self.send_request("schedules")#['regular']
+        return await self.send_request("schedules")['regular']
 
     async def get_ranked(self):
-        return await self.send_request("schedules")#['gachi']
+        return await self.send_request("schedules")['gachi']
 
     async def get_league(self):
-        return await self.send_request("schedules")#['league']
+        return await self.send_request("schedules")['league']
 
     async def get_salmon_detail(self):
-        return await self.send_request("coop-schedules")#['details']
+        return await self.send_request("coop-schedules")['details']
 
     async def get_salmon_schedule(self):
-        return await self.send_request("coop-schedules")#['schedules']
+        return await self.send_request("coop-schedules")['schedules']
 
     async def get_na_splatfest(self):
-        return await self.send_request("festivals.json")#['na']
+        return await self.send_request("festivals.json")['na']
 
-
-test = Splatnet()
-print(test.get_turf())
+    # TODO: fix [] error found in above functions
 
